e1c16ac36233e445d9f7cf1e631cdc836dd2cffc
Bhavna Jain <bhavnaostwal@gmail.com>
2022-04-30 22:13:06 -0400

    write function for generating read me

--- index.js
+++ index.js
@@ -20,48 +20,50 @@ const questions = [{
     type: "input",
     message: "What is the description of the project?",
     name: "description"
-}, {
-    type: "input",
-        message: "Table of Contents",
-        name: "contents"
 },{
     type: "input",
-        message: "What is the User Story?",
-        name: "user-story"
+    message: "What is the User Story?",
+    name: "user-story"
 },{ 
     type: "input",
-        message: "What are the installation instructions?",
-        name: "installation"
+    message: "What are the installation instructions?",
+    name: "installation",
+    default: 'npm i'
+},{ 
+    type: "input",
+    message: "What is the usage?",
+    name: "usage"
 },{
     type: "input",
-        message: "Are there any other contributors for this project?",
-        name: "contributors"
+    message: "Are there any other contributors for this project?",
+    name: "contributors"
 },{
     type: "input",
-        message: "What command should be run for testing?",
-        name: "test"
+    message: "What command should be run for testing?",
+    name: "test"
+        
 },{
     type: "list",
-        message: "What licenses should your project have?",
-        choices: ['MIT', 'GPL', 'APACHE', 'None'], 
-        name: "license"
+    message: "What licenses should your project have?",
+    choices: ['MIT', 'GPL 3.0', 'APACHE 2.0', 'None'], 
+    name: "license"
 }];
 
-// TODO: Create a function to write README file
+//  Create a function to write README file
 function writeToFile(fileName, data) {
      
  return fs.writeFile(`./output/${fileName}`, data, (err)=>
   err ? console.log(err) : console.log('Success!'));
 }
 
-// TODO: Create a function to initialize app
+//  Create a function to initialize app
 function init() {
     inquirer.prompt(questions)
             .then((answers)=> {
                 console.log('Generating README..');
                 writeToFile('README.md',generateMarkdown(answers));
                 
-            })
+            });
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,14 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+// function that returns a license badge based on which license is passed in
+// if there is no license, return an empty string
 function renderLicenseBadge(license) {
   if(license!= 'None'){
-   return `[![License](https://img.shields.io/badge/License-${license}-blue.svg)]`
+   return `[![License](https://img.shields.io/badge/License-${license}-blue.svg)]`;
     
   } else{
     return '';
 }};
 
-// TODO: Create a function that returns the license link
+//  function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if(license!= 'None'){
@@ -24,57 +24,59 @@ function renderLicenseLink(license) {
 function renderLicenseSection(license) {
   if(license!= 'None'){
     return `## License
-     This project is licensed under ${license} license.
+     This project is licensed under ${license} license.`
     
-    `
+    
    } else{
      return '';
  }};
 
 
 
-// TODO: Create a function to generate markdown for README
+//  function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ![Github License]${renderLicenseBadge(data.license)}
+
   
 ## Description
 ${data.description}
 
+# Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+
 ## User Story
-AS A traveler
-I WANT to see the weather outlook for multiple cities
-SO THAT I can plan a trip accordingly
-## Expected Goals
+${data.user-story}
 
-GIVEN a weather dashboard with form inputs
-WHEN I search for a city
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-WHEN I view the UV index
-THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-WHEN I click on a city in the search history
-THEN I am again presented with current and future conditions for that city
+# Installation
+To install necessary dependencies, run the following command : 
+<pre><code>${data.installation}</code></pre>
 
-## Steps taken
+# Usage
+${data.usage}
 
-* I have created an API key to access the openweathermap API which will give me the current weather conditions of a city.
-* As a valid city is searched, its temperature,humidity,uv index,wind speed, 5day forecast will be dynamically added on the cards on dashboard.
-* Local storage is used to store the searched cities, which are also displaying in the list.
+# License
+${renderLicenseSection()}
+ This application is made under ${data.license} License.
+
+# Contributors
+${data.contributors}
 
 
 ## License
 ${renderLicenseBadge(data.license)}
 
-## Screenshots
-
-![weather dashboard](./assets/images/Screenshot-1.png)
-
-## Deployed Application
-
-https://bhavnao.github.io/Weather-Dashboard/
+## Questions
+* If you have any questions about the repo, 
+[open an issue](https://github.com/${data.github}/${data.title}/issues/new) 
+or contact me directly at ${data.email}. 
+* You can find more of my work at [${data.github}](https://github.com/${data.github})
 
 `;
 }
